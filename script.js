c7b098d11a45e13fd6eb3fe4252daf86cf3f3280
Jacob MacDonald <100619060+MadDonald@users.noreply.github.com>
2023-02-01 17:26:22 -0500

    Uploading the completed javascript and tweaks made to HTML/CSS

--- script.js
+++ script.js
@@ -1,72 +1,78 @@
-//Variables for generation options
-var choiceArray = [];
-var characterLenght = [];
+var apiKey = "4124af60ae75a9ff36da60e4a5442d55";
+//lets us add data to the html file areas
+var weatherContainer = document.querySelector(".weather-container");
+var cityInput = document.querySelector("#city-input");
+var cityName = document.querySelector("#city-name");
+var weatherIcon = document.querySelector("#weather-icon");
+var temperature = document.querySelector("#temperature");
+var weatherDescription = document.querySelector("#weather-description");
+var forecast = document.querySelector("#forecast");
+var sidebar = document.querySelector(".sidebar");
 
-var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-var special = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"];
+weatherContainer.addEventListener("submit", function(event) {
+  event.preventDefault();
+  getWeather(cityInput.value);
+});
 
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
-
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
-// Write password to the #password input
-function writePassword() {
-    var correctOptions = getOptions(); //Checks if the user selected any options
-    var passwordText = document.querySelector("#password");
-    
-     if (correctOptions) {
-      var newPassword = generatePassword();
-
-      passwordText.value = newPassword;
-    } else {
-        passwordText.value = "Please select one option for a password";
-    }
+//The function to retrieve weather information
+async function getWeather(city) {
+  var url = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`;
+  var response = await fetch(url);
+  var data = await response.json();
+  saveSearch(city);
+  displayWeather(data);
 }
 
-function generatePassword() {
-  //Generates the password based on the options selected
-  var password = "";
-  for(var i = 0; i < characterLenght; i++) {
-    var randomChar = Math.floor(Math.random() * choiceArray.length);
-    password = password + choiceArray[randomChar];
+//Functiin for the 5 day forcast
+function displayWeather(data) {
+  cityName.textContent = data.city.name;
+  weatherIcon.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${data.list[0].weather[0].icon}@2x.png`
+  );
+  weatherIcon.setAttribute("alt", data.list[0].weather[0].description);
+  temperature.textContent = `Temperature: ${data.list[0].main.temp}°C`;
+  weatherDescription.textContent = data.list[0].weather[0].description;
+  forecast.innerHTML = "";
+  for (let i = 0; i < 6; i++) {
+    forecast.innerHTML += `
+      <div class="forecast">
+        <p>${data.list[i * 8].dt_txt.slice(0, 10)}</p>
+        <img src="http://openweathermap.org/img/wn/${
+          data.list[i * 8].weather[0].icon
+        }@2x.png" alt="${data.list[i * 8].weather[0].description}">
+        <p>${data.list[i * 8].main.temp}°C</p>
+        <p>${data.list[i * 8].weather[0].description}</p>
+      </div>
+    `;
   }
-  return password;
 }
 
-//Prompt for how many characters
-function getOptions(){
-    choiceArray = []; //Resets the array each time
-
-    characterLenght = parseInt(prompt("How many characters would you like? Please choose between 8 and 128"));
-
-  //Confirms the lenght
-      while(characterLenght<= 7 || characterLenght >= 129) {
-        alert("The password must be between 8-128 characters, try again");
-        characterLenght = (prompt("How many characters would you like? Please choose between 8 and 128"));
-    }
-    //Notifies the user how long the password will be
-        alert("Your password will be " + characterLenght + " characters long");
-
-  //Asks user for other options
-    if(confirm("Do you want uppercase letters in your password?")) {
-     choiceArray = choiceArray.concat(upperCase);
-    }
-
-    if(confirm("Do you want lowercase letters in your password?")) {
-      choiceArray = choiceArray.concat(lowerCase);
-    }
-
-    if(confirm("Do you want numbers in your password?")) {
-      choiceArray = choiceArray.concat(numbers);
-    }
-
-    if(confirm("Do you want special characters in your password?")) {
-      choiceArray = choiceArray.concat(special);
-    }    
-    return true;
+function saveSearch(city) {
+  let searches;
+  if (localStorage.getItem("searches") === null) {
+    searches = [];
+  } else {
+    searches = JSON.parse(localStorage.getItem("searches"));
+  }
+  searches.unshift(city);
+  localStorage.setItem("searches", JSON.stringify(searches));
+  displaySearches();
 }
+
+function displaySearches() {
+  sidebar.innerHTML = "";
+  let pastSearches = [];
+  if (localStorage.getItem("searches") !== null) {
+    pastSearches = JSON.parse(
+      localStorage.getItem("searches")
+    );
+  }
+  pastSearches.forEach(search => {
+    sidebar.innerHTML += `
+      <p class="search" onClick="getWeather('${search}')">${search}</p>
+    `;
+  });
+}
+
+displaySearches();
